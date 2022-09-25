c138d092032914511a1f49bcaefb52f49f9e34e3
diego-acevedo <dacevedom@live.com.mx>
2022-09-25 17:29:37 -0300

    Change test in MainTest

--- src/main/java/cl/uchile/dcc/finalreality/MainTest.java
+++ src/main/java/cl/uchile/dcc/finalreality/MainTest.java
@@ -1,8 +1,10 @@
 package cl.uchile.dcc.finalreality;
 
 import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
+import cl.uchile.dcc.finalreality.model.character.Enemy;
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
 import cl.uchile.dcc.finalreality.model.character.player.*;
+import cl.uchile.dcc.finalreality.model.weapon.*;
 
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
@@ -10,36 +12,82 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class MainTest {
 
-  public static void main(String[] args) throws InvalidStatValueException {
-    // Test de personajes
+  public static void main(String[] args) throws InvalidStatValueException, InterruptedException {
     BlockingQueue<GameCharacter> queue = new LinkedBlockingQueue<>();
     Random rnd = new Random();
     // Knights
+    System.out.println("---Knights---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter knight = new Knight("Knight %d".formatted(i), rnd.nextInt(100), 0, queue);
-      System.out.println(knight);
+      PlayerCharacter knight = new Knight("Knight %d".formatted(i), rnd.nextInt(100)+1, 0, queue);
+      Weapon weapon = new Sword("Sword %d".formatted(i), 0, rnd.nextInt(100)+1);
+      knight.equip(weapon);
+      System.out.printf("%s using %s\n", knight, weapon);
+      if (i==0) {
+        queue.put(knight);
+      }
     }
-    // Engineer
+    // Engineers
+    System.out.println("---Engineers---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter engineer = new Engineer("Engineer %d".formatted(i), rnd.nextInt(100), 0, queue);
-      System.out.println(engineer);
+      PlayerCharacter engineer = new Engineer("Engineer %d".formatted(i), rnd.nextInt(100)+1,
+          0, queue);
+      Weapon weapon = new Axe("Sword %d".formatted(i), 0, rnd.nextInt(100)+1);
+      engineer.equip(weapon);
+      System.out.printf("%s using %s\n", engineer, weapon);
+      if (i==0) {
+        queue.put(engineer);
+      }
     }
-    // Knights
+    // Thiefs
+    System.out.println("---Thiefs---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter thief = new Thief("Knight %d".formatted(i), rnd.nextInt(100), 0, queue);
-      System.out.println(thief);
+      PlayerCharacter thief = new Thief("Thief %d".formatted(i), rnd.nextInt(100)+1, 0, queue);
+      Weapon weapon = new Bow("Sword %d".formatted(i), 0, rnd.nextInt(100)+1);
+      thief.equip(weapon);
+      System.out.printf("%s using %s\n", thief, weapon);
+      if (i==0) {
+        queue.put(thief);
+      }
     }
-    // Knights
+    // Black Mages
+    System.out.println("---Black Mages---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter blackMage = new BlackMage("Knight %d".formatted(i), rnd.nextInt(100),
+      PlayerCharacter blackMage = new BlackMage("Black Mage %d".formatted(i), rnd.nextInt(100)+1,
           0, rnd.nextInt(100), queue);
-      System.out.println(blackMage);
+      Weapon weapon = new Staff("Sword %d".formatted(i), 0, rnd.nextInt(100)+1, 0);
+      blackMage.equip(weapon);
+      System.out.printf("%s using %s\n", blackMage, weapon);
+      if (i==0) {
+        queue.put(blackMage);
+      }
     }
-    // Knights
+    // White Mages
+    System.out.println("---White Mages---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter whiteMage = new WhiteMage("Knight %d".formatted(i), rnd.nextInt(100),
+      PlayerCharacter whiteMage = new WhiteMage("White Mage %d".formatted(i), rnd.nextInt(100)+1,
           0, rnd.nextInt(100), queue);
-      System.out.println(whiteMage);
+      Weapon weapon = new Knife("Sword %d".formatted(i), 0, rnd.nextInt(100)+1);
+      whiteMage.equip(weapon);
+      System.out.printf("%s using %s\n", whiteMage, weapon);
+      if (i==0) {
+        queue.put(whiteMage);
+      }
+    }
+    // Enemies
+    System.out.println("---Enemies---");
+    for (int i = 0; i < 5; i++) {
+      Enemy enemy = new Enemy("Enemy %d".formatted(i), rnd.nextInt(100)+1,
+          rnd.nextInt(100)+1, 0, queue);
+      System.out.println(enemy);
+      queue.put(enemy);
+    }
+    long startTime = System.currentTimeMillis();
+    while (System.currentTimeMillis()-startTime < 10000) {
+      if (!(queue.isEmpty())) {
+        GameCharacter character = queue.poll();
+        System.out.printf("%s attacked\n",character);
+        character.addToQueue();
+      }
     }
   }
 }
