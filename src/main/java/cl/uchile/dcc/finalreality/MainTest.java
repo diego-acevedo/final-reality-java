6a834790da402cbedc2cfe39ed58444d33595271
diego-acevedo <dacevedom@live.com.mx>
2022-09-27 22:21:06 -0300

    Add documentation to interface Weapon

--- src/main/java/cl/uchile/dcc/finalreality/MainTest.java
+++ src/main/java/cl/uchile/dcc/finalreality/MainTest.java
@@ -18,66 +18,66 @@ public class MainTest {
     // Knights
     System.out.println("---Knights---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter knight = new Knight("Knight %d".formatted(i), rnd.nextInt(100)+1, 0, queue);
-      Weapon weapon = new Sword("Sword %d".formatted(i), 0, rnd.nextInt(50)+50);
+      PlayerCharacter knight = new Knight("Knight %d".formatted(i), rnd.nextInt(100) + 1, 0, queue);
+      Weapon weapon = new Sword("Sword %d".formatted(i), 0, rnd.nextInt(50) + 50);
       knight.equip(weapon);
       System.out.printf("%s using %s\n", knight, weapon);
-      if (i==0) {
+      if (i == 0) {
         queue.put(knight);
       }
     }
     // Engineers
     System.out.println("\n---Engineers---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter engineer = new Engineer("Engineer %d".formatted(i), rnd.nextInt(100)+1,
+      PlayerCharacter engineer = new Engineer("Engineer %d".formatted(i), rnd.nextInt(100) + 1,
           0, queue);
-      Weapon weapon = new Axe("Sword %d".formatted(i), 0, rnd.nextInt(50)+50);
+      Weapon weapon = new Axe("Sword %d".formatted(i), 0, rnd.nextInt(50) + 50);
       engineer.equip(weapon);
       System.out.printf("%s using %s\n", engineer, weapon);
-      if (i==0) {
+      if (i == 0) {
         queue.put(engineer);
       }
     }
     // Thiefs
     System.out.println("\n---Thiefs---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter thief = new Thief("Thief %d".formatted(i), rnd.nextInt(100)+1, 0, queue);
-      Weapon weapon = new Bow("Sword %d".formatted(i), 0, rnd.nextInt(50)+50);
+      PlayerCharacter thief = new Thief("Thief %d".formatted(i), rnd.nextInt(100) + 1, 0, queue);
+      Weapon weapon = new Bow("Sword %d".formatted(i), 0, rnd.nextInt(50) + 50);
       thief.equip(weapon);
       System.out.printf("%s using %s\n", thief, weapon);
-      if (i==0) {
+      if (i == 0) {
         queue.put(thief);
       }
     }
     // Black Mages
     System.out.println("\n---Black Mages---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter blackMage = new BlackMage("Black Mage %d".formatted(i), rnd.nextInt(100)+1,
+      PlayerCharacter blackMage = new BlackMage("Black Mage %d".formatted(i), rnd.nextInt(100) + 1,
           0, rnd.nextInt(100), queue);
-      Weapon weapon = new Staff("Sword %d".formatted(i), 0, rnd.nextInt(50)+50, 0);
+      Weapon weapon = new Staff("Sword %d".formatted(i), 0, rnd.nextInt(50) + 50, 0);
       blackMage.equip(weapon);
       System.out.printf("%s using %s\n", blackMage, weapon);
-      if (i==0) {
+      if (i == 0) {
         queue.put(blackMage);
       }
     }
     // White Mages
     System.out.println("\n---White Mages---");
     for (int i = 0; i < 5; i++) {
-      PlayerCharacter whiteMage = new WhiteMage("White Mage %d".formatted(i), rnd.nextInt(100)+1,
+      PlayerCharacter whiteMage = new WhiteMage("White Mage %d".formatted(i), rnd.nextInt(100) + 1,
           0, rnd.nextInt(100), queue);
-      Weapon weapon = new Knife("Sword %d".formatted(i), 0, rnd.nextInt(50)+50);
+      Weapon weapon = new Knife("Sword %d".formatted(i), 0, rnd.nextInt(50) + 50);
       whiteMage.equip(weapon);
       System.out.printf("%s using %s\n", whiteMage, weapon);
-      if (i==0) {
+      if (i == 0) {
         queue.put(whiteMage);
       }
     }
     // Enemies
     System.out.println("\n---Enemies---");
     for (int i = 0; i < 5; i++) {
-      Enemy enemy = new Enemy("Enemy %d".formatted(i), rnd.nextInt(50)+50,
-          rnd.nextInt(100)+1, 0, queue);
+      Enemy enemy = new Enemy("Enemy %d".formatted(i), rnd.nextInt(50) + 50,
+          rnd.nextInt(100) + 1, 0, queue);
       System.out.println(enemy);
       queue.put(enemy);
     }
@@ -91,7 +91,7 @@ public class MainTest {
     System.out.println("\n---Turns example---\nCharacters will attack and wait for their next turn\n");
 
     long startTime = System.currentTimeMillis();
-    while (System.currentTimeMillis()-startTime < 10000) {
+    while (System.currentTimeMillis() - startTime < 10000) {
       if (!(queue.isEmpty())) {
         GameCharacter character = queue.poll();
         System.out.printf("%s attacked\n",character);
--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/Weapon.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Weapon.java
@@ -1,17 +1,27 @@
 package cl.uchile.dcc.finalreality.model.weapon;
 
+/**
+ * This represents a weapon equipable by a
+ * {@link cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter PlayerCharacter}.
+ *
+ * @author <a href="https://github.com/diego-acevedo">Diego Acevedo</a>
+ */
+
 public interface Weapon {
 
+  /**
+   * Returns this weapon's name.
+   */
   String getName();
 
+  /**
+   * Returns this weapon's damage.
+   */
   int getDamage();
 
+  /**
+   * Returns this weapon's weight.
+   */
   int getWeight();
-
-  boolean equals(Object o);
-
-  int hashCode();
-
-  String toString();
 
 }
