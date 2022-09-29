e2df3e888ecfd85601053b6c39613673d6543c04
diego-acevedo <dacevedom@live.com.mx>
2022-09-29 18:19:19 -0300

    Add tests to MainTest

--- src/main/java/cl/uchile/dcc/finalreality/MainTest.java
+++ src/main/java/cl/uchile/dcc/finalreality/MainTest.java
@@ -30,6 +30,87 @@ public class MainTest {
   public static void main(String[] args) throws InvalidStatValueException, InterruptedException {
     BlockingQueue<GameCharacter> queue = new LinkedBlockingQueue<>();
     Random rnd = new Random();
+
+    PlayerCharacter knight1 = new Knight("Knight", 100, 50, queue);
+    PlayerCharacter knight2 = new Knight("Knight", 100, 50, queue);
+    PlayerCharacter knight3 = new Knight("AlternativeKnight", 100, 50, queue);
+    PlayerCharacter knight4 = new Knight("Knight", 150, 50, queue);
+    PlayerCharacter knight5 = new Knight("Knight", 100, 100, queue);
+
+    PlayerCharacter thief1 = new Thief("Thief", 100, 50, queue);
+    PlayerCharacter thief2 = new Thief("Thief", 100, 50, queue);
+    PlayerCharacter thief3 = new Thief("AlternativeThief", 100, 50, queue);
+    PlayerCharacter thief4 = new Thief("Thief", 150, 50, queue);
+    PlayerCharacter thief5 = new Thief("Thief", 100, 100, queue);
+
+    PlayerCharacter engineer1 = new Engineer("Engineer", 100, 50, queue);
+    PlayerCharacter engineer2 = new Engineer("Engineer", 100, 50, queue);
+    PlayerCharacter engineer3 = new Engineer("AlternativeEngineer", 100, 50, queue);
+    PlayerCharacter engineer4 = new Engineer("Engineer", 150, 50, queue);
+    PlayerCharacter engineer5 = new Engineer("Engineer", 100, 100, queue);
+
+    PlayerCharacter blackmage1 = new BlackMage("BlackMage", 100, 50, 100, queue);
+    PlayerCharacter blackmage2 = new BlackMage("BlackMage", 100, 50, 100, queue);
+    PlayerCharacter blackmage3 = new BlackMage("AlternativeBlackMage", 100, 50, 100, queue);
+    PlayerCharacter blackmage4 = new BlackMage("BlackMage", 150, 50, 100, queue);
+    PlayerCharacter blackmage5 = new BlackMage("BlackMage", 100, 100, 100, queue);
+    PlayerCharacter blackmage6 = new BlackMage("BlackMage", 100, 50, 200, queue);
+
+    PlayerCharacter whitemage1 = new WhiteMage("WhiteMage", 100, 50, 100, queue);
+    PlayerCharacter whitemage2 = new WhiteMage("WhiteMage", 100, 50, 100, queue);
+    PlayerCharacter whitemage3 = new WhiteMage("AlternativeWhiteMage", 100, 50, 100, queue);
+    PlayerCharacter whitemage4 = new WhiteMage("WhiteMage", 150, 50, 100, queue);
+    PlayerCharacter whitemage5 = new WhiteMage("WhiteMage", 100, 100, 100, queue);
+    PlayerCharacter whitemage6 = new WhiteMage("WhiteMage", 100, 50, 200, queue);
+
+    // Equals
+
+    System.out.println("-----Testing equals() methods-----\n");
+
+    if (!(knight1.equals(knight1)) ||
+        !(knight1.equals(knight2)) ||
+           knight1.equals(knight3) ||
+           knight1.equals(knight4) ||
+           knight1.equals(knight5)) {
+      System.out.println("Method equals() fails for Knight class\n");
+    } else System.out.println("Method equals() worked correctly for Knight class\n");
+
+    if (!(engineer1.equals(engineer1)) ||
+        !(engineer1.equals(engineer2)) ||
+        engineer1.equals(engineer3) ||
+        engineer1.equals(engineer4) ||
+        engineer1.equals(engineer5)) {
+      System.out.println("Method equals() fails for Engineer class\n");
+    } else System.out.println("Method equals() worked correctly for Engineer class\n");
+
+    if (!(thief1.equals(thief1)) ||
+        !(thief1.equals(thief2)) ||
+        thief1.equals(thief3) ||
+        thief1.equals(thief4) ||
+        thief1.equals(thief5)) {
+      System.out.println("Method equals() fails for Thief class\n");
+    } else System.out.println("Method equals() worked correctly for Thief class\n");
+
+    if (!(blackmage1.equals(blackmage1)) ||
+        !(blackmage1.equals(blackmage2)) ||
+        blackmage1.equals(blackmage3) ||
+        blackmage1.equals(blackmage4) ||
+        blackmage1.equals(blackmage5) ||
+        blackmage1.equals(blackmage6)) {
+      System.out.println("Method equals() fails for BlackMage class\n");
+    } else System.out.println("Method equals() worked correctly for BlackMage class\n");
+
+    if (!(whitemage1.equals(whitemage1)) ||
+        !(whitemage1.equals(whitemage2)) ||
+        whitemage1.equals(whitemage3) ||
+        whitemage1.equals(whitemage4) ||
+        whitemage1.equals(whitemage5) ||
+        whitemage1.equals(whitemage6)) {
+      System.out.println("Method equals() fails for WhiteMage class\n");
+    } else System.out.println("Method equals() worked correctly for WhiteMage class\n");
+
+    System.out.println("-----Testing toString() methods and equip() method (incomplete implementation)-----\n");
+
     // Knights
     System.out.println("---Knights---");
     for (int i = 0; i < 5; i++) {
