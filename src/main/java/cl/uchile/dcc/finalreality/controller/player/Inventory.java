02cb8fa080d3e98e0391b477e4817fc1bea4d090
diego-acevedo <dacevedom@live.com.mx>
2022-11-25 11:59:33 -0300

    Add method to randomly initialize the inventory when created

--- src/main/java/cl/uchile/dcc/finalreality/controller/player/Inventory.java
+++ src/main/java/cl/uchile/dcc/finalreality/controller/player/Inventory.java
@@ -1,5 +1,12 @@
 package cl.uchile.dcc.finalreality.controller.player;
 
+import cl.uchile.dcc.finalreality.controller.factories.weapon.AbstractWeaponFactory;
+import cl.uchile.dcc.finalreality.controller.factories.weapon.AxeFactory;
+import cl.uchile.dcc.finalreality.controller.factories.weapon.BowFactory;
+import cl.uchile.dcc.finalreality.controller.factories.weapon.KnifeFactory;
+import cl.uchile.dcc.finalreality.controller.factories.weapon.StaffFactory;
+import cl.uchile.dcc.finalreality.controller.factories.weapon.SwordFactory;
+import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
 import cl.uchile.dcc.finalreality.model.weapon.Weapon;
 
 import java.util.ArrayList;
@@ -9,8 +16,18 @@ public class Inventory {
 
   private final List<Weapon> items;
 
-  public Inventory() {
+  public Inventory() throws InvalidStatValueException {
     this.items = new ArrayList<>();
+    AbstractWeaponFactory axeFactory = new AxeFactory();
+    AbstractWeaponFactory bowFactory = new BowFactory();
+    AbstractWeaponFactory knifeFactory = new KnifeFactory();
+    AbstractWeaponFactory staffFactory = new StaffFactory();
+    AbstractWeaponFactory swordFactory = new SwordFactory();
+    this.initialize(axeFactory);
+    this.initialize(bowFactory);
+    this.initialize(knifeFactory);
+    this.initialize(staffFactory);
+    this.initialize(swordFactory);
   }
 
   public void addItem(Weapon weapon) {
@@ -19,5 +36,12 @@ public class Inventory {
 
   public Weapon getWeapon(int n) {
     return items.get(n);
+  }
+
+  public void initialize(AbstractWeaponFactory factory)
+      throws InvalidStatValueException {
+    for (int i = 0; i < 4; i++) {
+      this.addItem(factory.create());
+    }
   }
 }
